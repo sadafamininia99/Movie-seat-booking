23a8204c2dabd1b42040b32e26380e63cd5c92de
= <aminicomputer96@gmail.com>
2021-05-15 18:28:37 +0430

    commit saved

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 
 
-const  container=document.querySelector('.container');
-const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+    const  container=document.querySelector('.container');
+    const seats=document.querySelectorAll('.row .seat:not(.occupied)');
 
 
-const count= document.getElementById('count');
-const total =document.getElementById('total');
-const movieSelect =document.getElementById('movie');
+    const count= document.getElementById('count');
+    const total =document.getElementById('total');
+    const movieSelect =document.getElementById('movie');
 
-const ticketPrice= movieSelect.nodeValue;
+    const ticketPrice = movieSelect.Value;
 
-console.log(ticketPrice);
+
+
+    // update total and count
+    
+    function updateSelectedCount(){
+        const selectedSeats =document.querySelectorAll('.row.seat.selected');
+  
+        
+        const selectedSeatsCount = selectedSeats.length;
+
+        count.innerText = selectedSeatsCount;
+        total.innerText = selectedSeatsCount*ticketPrice;
+
+    }
+
+    container.addEventListener('click', (e) =>{
+    if (
+    
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+    
+    )
+    {
+        e.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+    
+    });
