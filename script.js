c6b39cd6f9fd2500bcfb08f5499dace9e6236085
= <aminicomputer96@gmail.com>
2021-05-15 19:07:58 +0430

    add
    // Update total and count
    function updateSelectedCount() {
      const selectedSeats = document.querySelectorAll('.row .seat.selected');
    
      const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 
 
-    const  container=document.querySelector('.container');
-    const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+                const  container=document.querySelector('.container');
+                const seats=document.querySelectorAll('.row .seat:not(.occupied)');
 
 
-    const count= document.getElementById('count');
-    const total =document.getElementById('total');
-    const movieSelect =document.getElementById('movie');
+                const count= document.getElementById('count');
+                const total =document.getElementById('total');
+                const movieSelect =document.getElementById('movie');
 
-    const ticketPrice = movieSelect.Value;
+                let ticketPrice = +movieSelect.Value;
 
 
 
-    // update total and count
-    
-    function updateSelectedCount(){
-        const selectedSeats =document.querySelectorAll('.row.seat.selected');
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
   
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+                    const selectedSeatsCount = selectedSeats.length;
         
-        const selectedSeatsCount = selectedSeats.length;
+                    count.innerText = selectedSeatsCount;
+                    total.innerText = selectedSeatsCount*ticketPrice;
+                
+                }
 
-        count.innerText = selectedSeatsCount;
-        total.innerText = selectedSeatsCount*ticketPrice;
+            // Movie select event
+        movieSelect.addEventListener('change', e => {
+            ticketPrice = +e.target.value;
+            setMovieData(e.target.selectedIndex, e.target.value);
+            updateSelectedCount();
+        });
 
-    }
-
-    container.addEventListener('click', (e) =>{
-    if (
-    
-    e.target.classList.contains('seat') &&
-    !e.target.classList.contains('occupied')
-    
-    )
-    {
-        e.target.classList.toggle('selected');
-
-        updateSelectedCount();
-    }
-    
-    });
+            // Seat click event
+            container.addEventListener('click', e => {
+                if (
+                e.target.classList.contains('seat') &&
+                !e.target.classList.contains('occupied')
+                ) {
+                e.target.classList.toggle('selected');
+            
+                updateSelectedCount();
+                }
+            });
+            
