5008430fbc20037f517cb267fd1cc845b8674d23
= <aminicomputer96@gmail.com>
2021-05-15 17:24:55 +0430

    movie seat prj with js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const  container=document.querySelector('.container');
+const seats=document.querySelectorAll('.row .seat:not(.occupied)');
+
+
+const count= document.getElementById('count');
+const total =document.getElementById('total');
+const movieSelect =document.getElementById('movie');
+
+const ticketPrice= movieSelect.nodeValue;
+
+console.log(ticketPrice);
